ae9deacfc94a93a443b537140b66cccbd83ec6e9
Tomilola-ng <tee.o2809@gmail.com>
2024-04-26 00:28:01 +0100

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+    EMPLOYEE DATA GATHER
+'''
+
+import re
+import sys
+import requests
+
+ENDPOINT = "https://jsonplaceholder.typicode.com"
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            idx = int(sys.argv[1])
+            response = requests.get(f'{ENDPOINT}/users/{idx}').json()
+            results = requests.get(f'{ENDPOINT}/todos').json()
+            employee = response.get('name')
+            all_tasks = list(filter(lambda x: x.get('userId') == idx, results))
+            done_tasks = list(filter(lambda x: x.get('completed'), all_tasks))
+            print(
+                f'Employee {employee} is done with tasks({len(done_tasks)}/{len(tasks)}):'
+            )
+            if len(done_tasks) > 0:
+                for task in done_tasks:
+                    print(f'\t {task.get('title')}')
