3781f345243250feab2b3e6e5484c01260dfa908
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-05-12 22:55:51 +0100

    PUPPETTER

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# DEBUUGING WORDPESS BUG WITH PUPPET
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
